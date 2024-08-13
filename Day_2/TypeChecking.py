5a24f93c196b5286a317d087835c068b1252ef0b
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-12 23:10:59 -0400

    Split the contents of Type Checking and Type Errors into two separate files.

---
+++ Day_2/TypeChecking.py
@@ -0,0 +1,8 @@
+
+# type() function tells the user the data type of whatever is in the functions: Typechecking
+print(type("Alphabet soup"))
+print(type(252))
+print(type(3.141592))
+print(type(True))
+
+input("Hello?")
