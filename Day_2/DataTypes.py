6208e1df586f9c4e035ab7b7837f6e0bd668a51f
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-08-12 21:40:17 -0400

    Quick run through of the primitive data types

---
+++ Day_2/DataTypes.py
@@ -0,0 +1,21 @@
+# Subscripting - allows you to pull a character from that specific position. Like other languages, start with 0
+print("Hello"[2])
+
+# Negatives count from the last character
+print("Hello"[-3])
+
+# Strings
+print("123" + "456")
+
+# Integers - Whole Numbers
+print(123 + 456)
+
+# Large Integers - underscores are used in place of commas. Not read by the code (just for human consumption)
+print(123_456_789)
+
+# Float - Floating Point Numbers, has decimal places
+print(3.141592)
+
+# Boolean - True or False, 1 or 0
+print(True)
+print(False)
