ee3817c71b3a436eb26f0f9503b9ee0ec32f1773
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-08-12 21:27:41 -0400

    The first day project containing a combination of print statements, inputs, variable handling and string concatenations.

---
+++ Day_1/Day1Project.py
@@ -0,0 +1,8 @@
+#Task: Create a Band Name Generator that asks for the user's Hometown name and pet name
+#GO
+print("Welcome to the Band Name Generator!")
+city_name = input("What's the name of the city that you grew up in?\n")
+pet_name = input("What's your pet's name?\n")
+band_name = city_name + " " + pet_name
+
+print("Your band name could be " + band_name +"\nGood Luck out there, " + band_name + "!")
