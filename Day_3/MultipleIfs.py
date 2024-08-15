c42bade19e7cb36da967c89d020b5e9e8fcd4ff7
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-15 18:41:00 -0400

    Learned about Logical Operators, and made some logic adjustments to the MultipleIfs.py file

--- Day_3/MultipleIfs.py
+++ Day_3/MultipleIfs.py
@@ -8,9 +8,11 @@ if height >= 48:
     if age < 12:
         print("Child tickets are $5.")
         bill = 5
-    elif age < 18 and age >= 12:
+    elif age <= 18:
         print("Youth tickets are $7.")
         bill = 7
+    elif age >= 45 and age <= 55: # logical operator checking more than one condition
+        print("Everything is going to be ok. Have a free ride on us!")
     else:
         print("Adult tickets are $12.")
         bill = 12
