0a5fc5db004f3210d85516efcb50d7133f9e0ecf
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-15 08:02:11 -0400

    Learned about some Conditional Statements and Conditional Operators

---
+++ Day_3/IfElse.py
@@ -0,0 +1,8 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in inches?\n"))
+
+# ifelse conditions are separated by the indentation of the code
+if height >= 48:
+    print("You can ride the rollercoaster!")
+else:
+    print("You can not ride this coaster bucko.")
---
+++ Day_3/Modulo.py
@@ -0,0 +1,11 @@
+# Operator that divides the two digits and outputs the remainder of the expression
+
+# Check if a number is even or odd
+
+print("I can deduce that the whole number you input is even or odd!")
+user_number = int(input("What is your whole number?\n"))
+
+if (user_number % 2 == 0):
+    print("Seems like your number is even.")
+else:
+    print("Yup, you've got an odd number there.")
