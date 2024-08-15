359004c83e70612137fe547acdb0ced4b882931e
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-15 08:13:13 -0400

    Worked with multiple if statments, progressing down different routes and processes depending on the user's answers

---
+++ Day_3/MultipleIfs.py
@@ -0,0 +1,24 @@
+print("Welcome to the rollercoaster!")
+height = int(input("What is your height in inches?\n"))
+bill = 0
+
+if height >= 48:
+    print("You can ride the rollercoaster!")
+    age = int(input("What is your age?\n"))
+    if age < 12:
+        print("Child tickets are $5.")
+        bill = 5
+    elif age < 18 and age >= 12:
+        print("Youth tickets are $7.")
+        bill = 7
+    else:
+        print("Adult tickets are $12.")
+        bill = 12
+    
+    wants_photo = input("Do you want to have a photo taken? Type y for Yes and n for No.")
+    if wants_photo == "y":
+        bill += 3
+
+    print(f"Your final bill is ${bill}.")
+else:
+    print("You can not ride this coaster bucko.")
