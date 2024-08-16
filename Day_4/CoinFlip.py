3e0b627895cd7bab83c5b350d95122303e868cc8
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-08-16 17:11:39 -0400

    Finished the logic surrounding CoinFlip.py

--- Day_4/CoinFlip.py
+++ Day_4/CoinFlip.py
@@ -1,5 +1,11 @@
+import random
+
 # Print heads or tails
-coin_outcome = random.randint(0,1)
-print(coin_outcome)
+coin_outcome = random.randint(0,1) # heads = 0, tails = 1
 
 if coin_outcome == 0:
+    outcome = "Heads"
+else:
+    outcome = "Tails"
+
+print(f"You flip a coin, and the result is:\n{outcome}!")
