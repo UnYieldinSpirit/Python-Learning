ea13e276f3848578162635be5a798a4dfd8c2c51
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-17 15:02:05 -0400

    Created a Rock Paper Scissors game against a computer that randomly decides on their choice. Made it repeat until the player does not want to play again.

---
+++ Day_4/tempCodeRunnerFile.py
@@ -0,0 +1,21 @@
+if player_choice >= 3 or player_choice < 0:
+#     print("You didn't select anything... You Lose!")
+#     quit()
+
+# print(choices[player_choice])
+
+# computer_choice = random.randint(0, 2)
+
+# print(f"Computer chose:\n{choices[computer_choice]}")
+
+# # logic: 0 < 1 < 2 < 0
+# if player_choice == computer_choice:
+#     print(tie)
+# elif player_choice == 0 and computer_choice == 2:
+#     print(win)
+# elif player_choice == 2 and computer_choice == 0:
+#     print(lose)
+# elif player_choice < computer_choice:
+#     print(lose)
+# else:
+#     print(win)
