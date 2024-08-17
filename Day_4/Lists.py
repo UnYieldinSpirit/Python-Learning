ab486b400920023d349ebc223104bcb1c465fe62
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-17 11:02:05 -0400

    Created some code to showcase the ability to combine multiple lists into one list, lines 13-18 specifically

--- Day_4/Lists.py
+++ Day_4/Lists.py
@@ -6,6 +6,13 @@ states_of_america[3] = "Connecticut" # can alter any index
 
 print(states_of_america[3])
 
-states_of_america.append("West Virginia")
+states_of_america.append("West Virginia") # adds an item to the end of the 
 
 print(states_of_america[4])
+
+fruits = ["Apples", "Bananas", "Oranges", "Grapes", "Lemons", "Limes"]
+vegetables = ["Carrots", "Kale", "Watermelon", "Collard Greens","Broccoli"]
+
+edible = [fruits, vegetables]
+
+print(edible[1][3]) # should print the 4th item in the 2nd list
