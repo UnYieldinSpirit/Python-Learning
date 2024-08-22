0dcdd2fc8a9e7de3f0f9fe20c9576b2e5eb4c143
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-08-21 20:11:42 -0400

    Learned the basics of functions and while loops

---
+++ Day_6/Functions.py
@@ -0,0 +1,9 @@
+# I have been using functions from the beginning. The built-in functions! print() for example
+
+def my_function(): # this is how you start the process of creating a function
+    print("Hello")
+    print("Goodbye")
+
+my_function()
+
+# functions are used as a means to easily reuse code that is used frequently
---
+++ Day_6/whileLoop.py
@@ -0,0 +1,7 @@
+def countdown():
+    test_condition = 10
+    while test_condition >= 0: # a loop that executes until a condition is no longer met
+        print(test_condition)
+        test_condition -= 1
+
+countdown()
