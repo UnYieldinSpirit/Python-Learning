5d1eb86f41a6dc8e26a179accde4ae3a9cd70857
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-08-21 21:39:57 -0400

    Established a means of tracking the user's guessed letters and the revealed letters/positions as well

--- Day_7/Hangman.py
+++ Day_7/Hangman.py
@@ -2,12 +2,37 @@ import random
 
 word_bank = ["peanut", "alphabet", "caramel", "gideon", "flabbergasted"]
 comp_word = random.choice(word_bank)
+comp_word_list = []
+reveal_list = []
+guessed_letters = []
 num_guesses = 5
+correct = False # variable that tracks if the user's guess is correct
 
+# function that sets the list that handles displaying the revealed letters in the word. Ensures that the lengths are the same
+def set_reveal_list():
+    for space in range(len(comp_word)):
+        reveal_list.append("-")
+
+def modify_reveal_list(space, guess):
+    reveal_list[space] = guess
+
+def modify_guessed_letters(guess):
+    guessed_letters.append(guess)
+
+for char in comp_word:
+    comp_word_list.append(char)
+    
+set_reveal_list()
+
+print(reveal_list)
 print(comp_word)
+print(comp_word_list)
 
-print(f"Welcome to Hangman! You get a total of {num_guesses} guesses before you lose...\nDuplicate guesses do not count! Good luck")
+print(f"Welcome to Hangman! You get a total of {num_guesses} incorrect guesses before you lose...\nDuplicate guesses do not count! Good luck")
 
-while(num_guesses > 0):
+while(num_guesses > 0 and correct == False):
+    correct = False
     print(f"Number of guesses left: {num_guesses}")
+
+
     num_guesses -= 1
