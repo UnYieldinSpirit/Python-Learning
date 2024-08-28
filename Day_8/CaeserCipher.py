63ad9e1cef629f1194b1695e6a3064023f6bc948
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-28 19:08:44 -0400

    Added a file that holds the logo for caeser cipher ASCII art. Also added the ability to take inputs that are not only letters, they just don't get shifted with the letters.

--- Day_8/CaeserCipher.py
+++ Day_8/CaeserCipher.py
@@ -1,3 +1,6 @@
+import CaesarArt
+
+print(CaesarArt.logo)
 # program tasked with performing a caesar cipher on a message. ability to encrypt/decrypt
 
 alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
@@ -8,23 +11,29 @@ shift = int(input("Type the shift number:\n"))
 def encode(original_msg, shift):
     final_msg = ""
     for char in original_msg:
-        index_position = alphabet.index(char) # finds the letter in the text's alphabet position
-        index_position += shift # creates the shift in the letter index position
+        if char in alphabet: # uses this code to create the encoded letters, not numbers nor symbols.
+            index_position = alphabet.index(char) # finds the letter in the text's alphabet position
+            index_position += shift # creates the shift in the letter index position
 
-        while index_position > len(alphabet) - 1: # while the index_position is out of the bounds of the alphabet list...
-            index_position -= len(alphabet) # continuously subtracts the amount of the alphabet list until it is within the bounds of the 
+            while index_position > len(alphabet) - 1: # while the index_position is out of the bounds of the alphabet list...
+                index_position -= len(alphabet) # continuously subtracts the amount of the alphabet list until it is within the bounds of the 
 
-        char = alphabet[index_position] # replaces the letter with the shifted letter
-        final_msg += char # creates the encoded msg
+            char = alphabet[index_position] # replaces the letter with the shifted letter
+            final_msg += char
+        else: # does nothing but instead just tacks the stuff into the final message wherever in the loop we are
+            final_msg += char # adds the character to the encoded message
     return final_msg
 
 def decode(original_msg, shift):
     final_msg = ""
     for char in original_msg:
-        index_position = alphabet.index(char) # finds the letter in the text's alphabet position
-        index_position -= shift # creates the shift in the letter index position
-        char = alphabet[index_position] # replaces the letter with the shifted letter
-        final_msg += char # creates the decode msg
+        if char in alphabet:
+            index_position = alphabet.index(char) # finds the letter in the text's alphabet position
+            index_position -= shift # creates the shift in the letter index position
+            char = alphabet[index_position] # replaces the letter with the shifted letter
+            final_msg += char # creates the decode msg
+        else:
+            final_msg += char
     return final_msg
 
 def casear(original_msg, shift, direction):  
