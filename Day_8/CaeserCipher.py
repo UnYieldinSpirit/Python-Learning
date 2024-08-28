2e0dabc69a57185d53f4171709df9e2ec8dc7cae
UnYieldinSpirit <unyieldinspirit@gmail.com>
2024-08-28 14:30:59 -0400

    Created code to encode and decode the messages a user submits, there are some issues with the encode logic. If the cipher passes 'Z' then there is some logic errors.

--- Day_8/CaeserCipher.py
+++ Day_8/CaeserCipher.py
@@ -1,10 +1,36 @@
 # program tasked with performing a caesar cipher on a message. ability to encrypt/decrypt
 
-alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+alphabet= ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n").lower()
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
 
-def encode():
+def encode(original_msg, shift):
+    final_msg = ""
+    for char in original_msg:
+        index_position = alphabet.index(char) # finds the letter in the text's alphabet position
+        index_position += shift # creates the shift in the letter index position
+        if index_position > len(alphabet) - 1:
+            
+        char = alphabet[index_position] # replaces the letter with the shifted letter
+        final_msg += char # creates the encoded msg
+    return final_msg
 
-def decode():
+def decode(original_msg, shift):
+    final_msg = ""
+    for char in original_msg:
+        index_position = alphabet.index(char) # finds the letter in the text's alphabet position
+        index_position -= shift # creates the shift in the letter index position
+        char = alphabet[index_position] # replaces the letter with the shifted letter
+        final_msg += char # creates the decode msg
+    return final_msg
+
+# maybe find the index number that the letter is and then replace that letter with one of an index higher. Compensate for going over the index as well.
+#.index()
+
+if direction == "encode":
+    final_msg = encode(text, shift)
+else:
+    final_msg = decode(text, shift)
+
+print(final_msg)
---
+++ Day_8/tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+for char in text:
+#     index_position = alphabet.index(char) # finds the letter in the text's alphabet position
+#     index_position += shift
+#     print(index_position)
+#     char = alphabet[index_position]
+#     encoded_msg += char
