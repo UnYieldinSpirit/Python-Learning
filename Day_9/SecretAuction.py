cc18d47f0f0b944ee73e38ce1276ee48b80033ea
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-08-29 11:13:53 -0400

    Finished the SecretAuction.py program without error or improper input handling.

--- Day_9/SecretAuction.py
+++ Day_9/SecretAuction.py
@@ -9,21 +9,29 @@ auctions = {}
 
 def auction_inputs(dict, key, value):
      dict[key] = value
-     return
 
 def calculate_winner(dict):
-     print(dict)
+     top_bid = 0
+     top_bidder = ""
 
-def clear():
+     for key in dict:
+        if dict[key] > top_bid: # compares each bid with the current top bid and sets the higher of the two as the top bid
+             top_bid = dict[key]
+             top_bidder = key
+
+     print(f"The winner with the highest bid of ${dict[top_bidder]}, is {top_bidder}!")
+              
+def clear(): # artificial clear of the terminal
      print("\n" *100)
 
 proceed = True
 
-while proceed == True:
+while proceed == True: # coninues the loop until no more entries are entered
     name = input("What is your name? ")
-    bid = input("What is your bid? $")
+    bid = int(input("What is your bid? $"))
     auction_inputs(auctions, name, bid)
     answer = input("Are there any more bidders? (y/n) ")
+
     if answer == 'y':
          clear()
     else:
