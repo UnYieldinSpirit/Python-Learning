404005a474957271f338fea7978baae9041b4bbc
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-09-02 12:54:06 -0400

    Added the ability for the program to track the numbers that the original is divisible by and report them back to the user.

--- Day_12/PrimeNumberTester.py
+++ Day_12/PrimeNumberTester.py
@@ -1,7 +1,13 @@
 def is_prime(num):
+    divisible_by = [1]
+    prime = True
+
     for number in range(2, num):
        if num % number == 0:
-           return False
-    return True
+           divisible_by.append(number)
+           prime = False
+    divisible_by.append(num)
+    print(f"Divisible by: {divisible_by}")
+    return prime
 
 print(is_prime(int(input("Enter a number to test whether it is prime: "))))
