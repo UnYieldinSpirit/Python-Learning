9387087090a36c47b43c4e2afaa448e34384f449
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-09-02 13:50:56 -0400

    Began the creation of a number guessing game.

---
+++ Day_12/NumberGuess.py
@@ -0,0 +1,7 @@
+import random
+
+# need a random number
+# check systems to tell the player whether they are above or below the number
+comp_num = random.choice(range(0,101))
+print("I'm thinking of a number between 1 and 100.")
+difficulty = input("Choose a difficulty. Type 'easy' or 'hard': ")
