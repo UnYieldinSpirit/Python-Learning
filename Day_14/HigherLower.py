227238d818cd31ffe72544064f942f9d04d2fae4
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2024-09-07 12:52:19 -0400

    Completed the HigherLower.py game and added documentation for all of the functions to better explain their purpose.

--- Day_14/HigherLower.py
+++ Day_14/HigherLower.py
@@ -1,3 +1,13 @@
+"""DRY CODE IS BETTER THAN WET CODE
+Don't
+Repeat
+Yourself
+
+We
+Enjoy
+Typing
+"""
+
 import random
 import art
 import gamedata
@@ -13,13 +23,22 @@ You are tasked with deciding which one has the higher amount of followers.
 Get it right and you move on, get it wrong and you lose your streak."""
 
 def title():
+    """prints the HigherLower art title"""
     print(art.logo)
 
 def vs():
+    """prints the HigherLower vs art"""
     print(art.vs)
 
+def reset_params():
+    """resets the parameters of the game to ensure that the game restarts"""
+    global streak
+    global loss_condition
+    streak = 0
+    loss_condition = False
+
 def competitors():
-    """Sets the two options that are used for the game"""
+    """sets the two options that are used for the game"""
     global choice1
     global choice2
     num1 = random.randrange(0, len(gamedata.data) - 1)
@@ -31,12 +50,12 @@ def competitors():
     choice2 = gamedata.data[num2]
 
 def loss():
-    """Sets the loss condition to true to initiate the endgame function"""
+    """sets the loss condition to true to initiate the endgame function"""
     global loss_condition
     loss_condition = True
 
 def compare(user_input):
-    """Compares the two options against each other to decide which one has the most followers and which choice is the correct choice as a result"""
+    """compares the two options against each other to decide which one has the most followers and which choice is the correct choice as a result"""
     global loss
     global correct_choice
     global streak
@@ -52,6 +71,7 @@ def compare(user_input):
         loss()
 
 def user_choice_game():
+    """ensures that the user inputs are valid"""
     user = input("Who has more followers? Type 'A' or 'B': ").upper()
     if 'A' != user != 'B':
         print("That is not a valid choice")
@@ -61,6 +81,7 @@ def user_choice_game():
         return user
 
 def user_choice_play_again():
+    """ensures that the user inputs are valid"""
     user = input("Play Again? Type 'Y' or 'N': ").upper()
     if 'Y' != user != 'N':
         print("That is not a valid choice")
@@ -70,22 +91,30 @@ def user_choice_play_again():
         return user
 
 def send_options(info):
+    """populates the choice with the appropriate information"""
     return f"{info['name']}, a {info['description']}, from {info['country']}"
 
 def endgame():
     """Initiates the game ending when the player loses"""
     global streak
     print(f"You got {streak} correct!")
+    if user_choice_play_again() == 'Y':
+        reset_params()
+        game()
+    else:
+        print(f"Thank you for playing! Let's see if you can get a higher streak than {streak} next time!")
 
 def game():
-    title()
-    competitors()
-    print(f"Choice A: {send_options(choice1)}")
-    vs()
-    print(f"Choice B: {send_options(choice2)}")
-    compare(user_choice_game())
+    """contains all of the functionality of the game"""
+    global loss_condition
+    while loss_condition == False:
+        title()
+        competitors()
+        print(f"Choice A: {send_options(choice1)}")
+        vs()
+        print(f"Choice B: {send_options(choice2)}")
+        compare(user_choice_game())
+    endgame()
 
 print(game_prompt)
-while loss_condition == False:
-    game()
-endgame()
+game()
