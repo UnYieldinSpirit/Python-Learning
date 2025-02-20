8790bb44c1ee4990a33508a91a7136c05db8f14e
UnYieldinSpirit <61170718+UnYieldinSpirit@users.noreply.github.com>
2025-02-19 23:41:39 -0500

    Added some control aspects, but it needs to be tested. Still no mechanic that keeps the snake constantly moving.

--- Day_20 Snake Game Pt 1/main.py
+++ Day_20 Snake Game Pt 1/main.py
@@ -7,13 +7,41 @@ screen.title("Snaked Up")
 
 # set three turtles to squares in a row, 1st at (0,0)
 
+game = "test"
 snake_body = []
 starting_positions = [(0, 0), (-20, 0), (-40, 0)]
 
 for position in starting_positions:
     turtle = Turtle("square")
+    turtle.penup()
     turtle.color("white")
     turtle.goto(position)
     snake_body.append(turtle)
+
+def move():
+    for i in snake_body:
+        i.forward(5)
+
+def north():
+    for i in snake_body:
+        i.setheading("north")
+
+def east():
+    for i in snake_body:
+        i.setheading("east")
+
+def south():
+    for i in snake_body:
+        i.setheading("south")
+
+def west():
+    for i in snake_body:
+        i.setheading("west")
+
+
+screen.onkey(key = "Up", fun = north)
+screen.onkey(key = "Left", fun = west)
+screen.onkey(key = "Right", fun = east)
+screen.onkey(key = "Down", fun = south)
 
 screen.exitonclick()
